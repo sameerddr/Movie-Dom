ecee144ed9be86646089855c1a698661c9dbea0f
Sameer <sameer.writes.70@gmail.com>
2022-08-28 21:30:07 +0530

    movie-dom

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Working");
+
+let search = document.querySelector("#search");
+let container = document.querySelector(".container");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("checking");
+    fetch(`https://api.tvmaze.com/search/shows?q==${search.value}`)
+        .then(response => response.json())
+        .then(data => {
+            search.value = " ";
+            for (let i = 0; i < data.length; i++) {
+                let res = `<div class="content" >
+                <img src= " ${data[i].show.image}" class="img" >
+                <h1>Name :    &nbsp;
+                ${data[i].show.name}</h1>
+                <h3>Language: ${data[i].show.language}</h3>
+                <h3>Start-date: ${data[i].show.premiered}</h3>
+                <button class="deatails"> More Info </button>
+                </div>`
+                container.innerHTML += res;
+            }
+            console.log(data);
+        })
+
+    .catch(err => alert("Wrong Data Name!"));
+})
